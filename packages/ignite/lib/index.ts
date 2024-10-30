d86c0752c99b2cc23af60bfd2587247e165de8a9
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-10-30 17:51:56 +0330

    feat: exported the main init function

--- packages/ignite/lib/index.ts
+++ packages/ignite/lib/index.ts
@@ -1,4 +1,13 @@
+import { useInitConfig } from "$lib/config";
+
 export * from "./auth";
 export * from "./config";
 export * from "./env";
 export * from "./stores";
+
+/**
+ * initialized the app, including the theme, language and the api config
+ */
+export function useInit() {
+	return useInitConfig();
+}
