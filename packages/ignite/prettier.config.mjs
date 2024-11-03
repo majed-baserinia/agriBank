a12d112aca3d9255169b6459290d00805c1d9cc4
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-03 12:17:11 +0330

    chore: correct name for prettier configs

---
+++ packages/ignite/prettier.config.mjs
@@ -0,0 +1,10 @@
+import htscPrettierConfig from "@htsc/prettier-config";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	...htscPrettierConfig
+};
+
+export default config;
---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	endOfLine: "auto",
+	printWidth: 100,
+	requirePragma: false,
+	semi: true,
+	tabWidth: 2,
+	trailingComma: "none",
+	useTabs: true,
+	singleAttributePerLine: true
+};
+
+export default config;
