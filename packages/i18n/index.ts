a3507fa94202f0a8986b3cfa7c082712307ad5ea
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-11 08:32:46 +0330

    fix: correctly export root level d.ts module exports

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -1,1 +1,16 @@
 export * from "./lib";
+
+import "i18next";
+import type { BaseLanguageOptions } from "./lib";
+
+export type ModifiedHTSCTypeOptions<T> = T extends { defaultNS: string }
+	? Omit<BaseLanguageOptions, "defaultNS"> & T
+	: BaseLanguageOptions & T;
+
+declare module "i18next" {
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	export interface HTSCTypeOptions {}
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	interface CustomTypeOptions extends ModifiedHTSCTypeOptions<HTSCTypeOptions> {}
+}
--- packages/ui/lib/components/i18.d.ts
+++ packages/ui/lib/components/i18.d.ts
@@ -1,7 +1,1 @@
-import type { BaseLanguageOptions } from "@htsc/i18n";
-import "i18next";
-
-declare module "i18next" {
-	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-	interface CustomTypeOptions extends BaseLanguageOptions {}
-}
+import "@htsc/i18n";
