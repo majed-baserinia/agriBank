d15d303d55623ce9a53253651ba2843661d1916f
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-18 13:36:46 +0330

    feat: completed template cli

--- packages/cli/src/create-template/cli.ts
+++ packages/cli/src/create-template/cli.ts
@@ -1,12 +1,15 @@
-import { Command, program } from "commander";
+import { Command, Option, program } from "commander";
 import { z } from "zod";
 import { generate } from "./generate";
 
-export const optionsSchema = z.object({});
+export const optionsSchema = z.object({
+	out: z.string()
+});
 
 export function setupCommand() {
 	const command = new Command("create");
 	program.addCommand(command);
+	command.addOption(new Option("--out <string>", "output directory").default("./"));
 	command.action((options) => {
 		call(options);
 	});
--- packages/cli/src/create-template/generate.ts
+++ packages/cli/src/create-template/generate.ts
@@ -1,1 +1,13 @@
-export function generate(options: object) {}
+import type { optionsSchema } from "$/create-template/cli";
+import { sparsCheckout } from "$/create-template/git";
+import type { z } from "zod";
+
+export function generate(options: z.infer<typeof optionsSchema>) {
+	sparsCheckout({
+		branch: "main",
+		outDir: options.out,
+		repoUrl: "https://github.com/FoHoOV/htsc.git",
+		sparsePath: "templates/default",
+		tempDir: "./htsc-tmp"
+	});
+}
