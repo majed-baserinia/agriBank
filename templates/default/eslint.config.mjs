603793e411376d56bd7a4febba8e418edee4416d
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-19 09:12:55 +0330

    chore: improve template

--- templates/default/eslint.config.mjs
+++ templates/default/eslint.config.mjs
@@ -4,15 +4,11 @@ import htsceslint from "@htsc/eslint-plugin";
 
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-	...htsceslint.configs["flat/recommended"],
-	...pluginRouter.configs["flat/recommended"],
-	{
-		languageOptions: {
-			parserOptions: {
-				tsconfigRootDir: import.meta.url,
-				project: "./tsconfig.eslint.json"
-			}
+export default tseslint.config(...htsceslint.configs["flat/recommended"], {
+	languageOptions: {
+		parserOptions: {
+			tsconfigRootDir: import.meta.url,
+			project: "./tsconfig.eslint.json"
 		}
 	}
-);
+});
