39bc091a6a51abf1048bd122b83a4780f380f68f
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-12-08 09:10:27 +0330

    feat: separate app/node tsconfig files

--- packages/i18n/eslint.config.mjs
+++ packages/i18n/eslint.config.mjs
@@ -4,8 +4,8 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(...htsceslint.configs["flat/recommended"], {
 	languageOptions: {
 		parserOptions: {
-			project: ["./tsconfig.eslint.json"],
-			tsconfigRootDir: import.meta.dirname
+			tsconfigRootDir: import.meta.dirname,
+			project: "./tsconfig.json"
 		}
 	}
 });
