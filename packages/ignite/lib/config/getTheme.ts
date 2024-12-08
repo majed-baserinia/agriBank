6443d55fb4ab9161ea26616bb437d72f1baba7c8
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-12-08 21:51:43 +0330

    fix: don't ignore theme while erroring out on fetching palette

--- packages/ignite/lib/config/getTheme.ts
+++ packages/ignite/lib/config/getTheme.ts
@@ -2,6 +2,18 @@ import { type ThemeOptions } from "@mui/material";
 
 export async function getTheme(themeUrl: string, themeName: string) {
 	try {
+		const theme = await getBaseTheme();
+		const palette = await getCustomPalette(themeUrl, themeName);
+		theme.palette = palette;
+		return theme;
+	} catch (error) {
+		console.error("error while creating theme with custom palette", error);
+		return {};
+	}
+}
+
+export async function getBaseTheme() {
+	try {
 		let theme: ThemeOptions = {};
 
 		if (import.meta.dynamic.env.DEV) {
@@ -15,14 +27,21 @@ export async function getTheme(themeUrl: string, themeName: string) {
 			theme = (await (await fetch("/default-theme.json")).json()) as ThemeOptions;
 		}
 
-		const url = `${themeUrl}${themeName ? themeName : "light"}.json`;
-		const customPalette = (await (await fetch(url)).json()) as ThemeOptions;
-
-		theme.palette = customPalette.palette;
-
 		return theme;
 	} catch (error) {
 		console.error("error while fetching theme", error);
+		return {};
+	}
+}
+
+export async function getCustomPalette(themeUrl: string, themeName: string) {
+	try {
+		const url = `${themeUrl}${themeName ? themeName : "light"}.json`;
+		const customPalette = (await (await fetch(url)).json()) as ThemeOptions;
+
+		return customPalette.palette;
+	} catch (error) {
+		console.error("error while fetching palette", error);
 		return {};
 	}
 }
