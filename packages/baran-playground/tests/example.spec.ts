2fa06e386b8e1b75d577da4465575cce3067c582
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2025-01-19 14:06:56 +0330

    test(baran-playground): use correct title

--- packages/baran-playground/tests/example.spec.ts
+++ packages/baran-playground/tests/example.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
 	await page.goto("/?Auth=false");
-	await expect(page).toHaveTitle("/baran-playground");
+	await expect(page).toHaveTitle("BARAN Playground");
 });
