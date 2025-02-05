e4c9b979865a61e3228abbaef0c290d542f830db
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2025-02-05 15:03:04 +0330

    chore(template): use correct selector imports in test example

--- templates/tanstack-router/tests/routes/index/example.spec.ts
+++ templates/tanstack-router/tests/routes/index/example.spec.ts
@@ -1,11 +1,11 @@
 import { test } from "$tests/fixtures/micro";
-import { createTestIdSelector } from "$tests/selectors";
+import { createTestIdForAgriComponent } from "$tests/selectors";
 
 test("show text on clicking say hello!", async ({ micro }) => {
 	await micro.goto("/");
 	const iframe = await micro.iframe();
 	await iframe.getByTestId("hello-world").click();
-	await iframe.waitForSelector(createTestIdSelector("alerts", { isAgriBankComponent: true }), {
+	await iframe.waitForSelector(createTestIdForAgriComponent("alerts"), {
 		state: "visible"
 	});
 });
