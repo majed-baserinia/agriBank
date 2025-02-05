a5a60fbb3a47c1d8f09626bbbdae7a0ed8828440
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2025-02-05 14:35:28 +0330

    chore(template): export different testid selectors for agricomponents and other components

--- templates/tanstack-router/tests/selectors/test-id.ts
+++ templates/tanstack-router/tests/selectors/test-id.ts
@@ -1,11 +1,11 @@
-export function createTestIdSelector(
-	id: string,
-	options?: {
-		isAgriBankComponent?: boolean;
-	}
-) {
-	if (options?.isAgriBankComponent) {
-		return `[data-testid="agribank-ui-${id}"]`;
-	}
+export function createTestIdSelector(id: string) {
 	return `[data-testid="${id}"]`;
+}
+
+export function createTestIdSelectorForAgriComponent(name: string) {
+	return createTestIdSelector(`agribank-ui-${name}`);
+}
+
+export function createTestIdForAgriComponent(name: string) {
+	return `agribank-ui-${name}`;
 }
