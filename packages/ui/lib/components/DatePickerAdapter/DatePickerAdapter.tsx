94289a4cfd32f4d2e984ca86339adf1d408c2b10
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2025-03-03 11:55:25 +0330

    fix(ui): datepicker now uses shorthand version of weekday names and also improved the styling abit

--- packages/ui/lib/components/DatePickerAdapter/DatePickerAdapter.tsx
+++ packages/ui/lib/components/DatePickerAdapter/DatePickerAdapter.tsx
@@ -4,7 +4,6 @@ import { InputAdapter } from "$components/InputAdapter";
 import { useEffect, useRef, useState } from "react";
 import { default as persian, default as persian_ca } from "react-date-object/calendars/persian";
 import persian_en from "react-date-object/locales/persian_en";
-import persian_fa from "react-date-object/locales/persian_fa";
 import { useTranslation } from "react-i18next";
 import DatePicker, { DateObject } from "react-multi-date-picker";
 import Icon from "react-multi-date-picker/components/icon";
@@ -16,12 +15,12 @@ import "react-multi-date-picker/styles/layouts/prime.css";
 
 import "./styles.css";
 
+import { modifiedPersianFa } from "./modified-persian-locals";
 import type { Props } from "./types";
 
-export function DatePickerAdapter(props: Props) {
+export function DatePickerAdapter({ label, helperText, onChange, error, defaultValue, sx }: Props) {
 	const { t, i18n } = useTranslation("base");
 
-	const { label = t("date"), helperText, onChange, error, defaultValue } = props;
 	const [value, setValue] = useState<Value>();
 	const datepicker = useRef(null);
 
@@ -40,13 +39,12 @@ export function DatePickerAdapter(props: Props) {
 			i18n.language === "fa-IR"
 				? new DateObject(date! as string).convert(persian, persian_en).format()
 				: new DateObject(date! as string).format();
-		//	setValue(date);
 		handleSyncValue(val);
 	};
 
 	const handleSyncValue = (val: string) => {
 		setValue(val);
-		onChange(val);
+		onChange?.(val);
 	};
 
 	return (
@@ -58,7 +56,7 @@ export function DatePickerAdapter(props: Props) {
 					className="primary rmdp-mobile"
 					format={"YYYY/MM/DD"}
 					inputMode="numeric"
-					locale={i18n.language === "fa-IR" ? persian_fa : undefined}
+					locale={i18n.language === "fa-IR" ? modifiedPersianFa : undefined}
 					monthYearSeparator=" "
 					onChange={(value) => handleDatePickerChange(value)}
 					ref={datepicker}
@@ -68,48 +66,10 @@ export function DatePickerAdapter(props: Props) {
 			}
 			error={error}
 			helperText={helperText}
-			label={label}
+			label={label ?? t("date")}
 			onChange={handleInputChange}
 			type="date"
+			sx={sx}
 		/>
 	);
-}
-
-{
-	/* <DatePicker
-				ref={datepicker}
-				render={
-					<>
-					</>
-					// <div onClick={() => setOpen(true)} >sdfsdf</div>
-				}
-				calendar={appLanguage === 'fa-IR' ? persian_ca : undefined}
-				locale={appLanguage === 'fa-IR' ? persian_fa : undefined}
-				value={value}
-				monthYearSeparator=" "
-				inputMode="numeric"
-				className="primary"
-				style={{ width: '100%' }}
-				// className="primary rmdp-mobile"
-
-				inputClass={`${error ? 'error' : ''} datepickerInput`}
-				hideOnScroll
-				calendarPosition={'top-right'}
-				placeholder={placeHolder}
-				onChange={(value) => handleChange(value)}
-				format={'YYYY/MM/DD'}
-				onClose={() => setOpen(false)}
-				onOpen={open ? () => true : () => false}
-			/> */
-}
-{
-	/* <FormHelperText
-				error={error}
-				sx={{
-					paddingRight: settings.language === 'en-GB' ? '16px' : 'unset',
-					paddingLeft: settings.language === 'fa-IR' ? '16px' : 'unset'
-				}}
-			>
-				{helperText}
-			</FormHelperText> */
 }
--- packages/ui/lib/components/DatePickerAdapter/types.ts
+++ packages/ui/lib/components/DatePickerAdapter/types.ts
@@ -1,8 +1,11 @@
+import type { SxProps } from "@mui/material";
+
 export type Props = {
 	className?: string;
 	defaultValue?: string;
 	error?: boolean;
 	helperText?: string;
 	label?: string;
-	onChange: (value: string) => void;
+	onChange?: (value: string) => void;
+	sx?: SxProps;
 };
