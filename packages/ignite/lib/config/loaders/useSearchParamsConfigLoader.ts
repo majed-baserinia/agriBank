adc022d76bac141a4673e23fc2877657bc6ad87e
Majed Baserinia <majed.baserinia.1988@gmail.com>
2025-05-25 11:28:11 +0330

    chore: fix default language selection logic

--- packages/ignite/lib/config/loaders/useSearchParamsConfigLoader.ts
+++ packages/ignite/lib/config/loaders/useSearchParamsConfigLoader.ts
@@ -2,7 +2,7 @@ import type { useRouter } from "$lib/facade/router";
 import { z } from "zod";
 
 export const searchParamsConfigSchema = z.object({
-	Lang: z.string({ coerce: true }).default("fa-IR"),
+	Lang: z.string({ coerce: true }).default(""),
 	Theme: z.string({ coerce: true }).toLowerCase().default("light"),
 	/**
 	 * if the app requires a postmessage from parent or not, ie if its `false` we don't require a postmessage
