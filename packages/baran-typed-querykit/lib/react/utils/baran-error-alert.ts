e3ba37e1da31042d959fadbbb828a04d8ada19ef
Majed Baserinia <majed.baserinia.1988@gmail.com>
2025-07-20 10:26:34 +0330

    chore: bump package version to 10.1.6

--- packages/baran-typed-querykit/lib/react/utils/baran-error-alert.ts
+++ packages/baran-typed-querykit/lib/react/utils/baran-error-alert.ts
@@ -1,10 +1,7 @@
 import { isBaranClientResult } from "$/types";
 import { pushAlert } from "@agribank/ui/stores/alerts";
-import { useTranslation } from "react-i18next";
 
 export function alertBaranError(result: unknown, throwError: boolean = false) {
-	const { t } = useTranslation("base");
-
 	if (!isBaranClientResult(result)) {
 		return;
 	}
@@ -24,10 +21,7 @@ export function alertBaranError(result: unknown, throwError: boolean = false) {
 			pushAlert({
 				type: "error",
 				messageText: result.error.message,
-				hasConfirmAction: true,
-				variantConfirm: "contained",
-				colorConfirm: "warning",
-				confirmButtonText: t("i-understand"),
+				hasConfirmAction: true
 			});
 	}
 }
