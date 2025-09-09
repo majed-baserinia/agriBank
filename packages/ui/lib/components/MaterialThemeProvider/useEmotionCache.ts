492be4d71fca89a6c8d148196dbc42938679065a
Majed Baserinia <majed.baserinia.1988@gmail.com>
2025-09-09 11:48:51 +0330

    chore: bump package version to 6.6.0

--- packages/ui/lib/components/MaterialThemeProvider/useEmotionCache.ts
+++ packages/ui/lib/components/MaterialThemeProvider/useEmotionCache.ts
@@ -3,18 +3,11 @@ import { useTranslation } from "react-i18next";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
 
-const rtlCache = createCache({
-	key: "muirtl",
-	stylisPlugins: [prefixer, rtlPlugin]
-});
-
-const ltrCache = createCache({ key: "muiltr" });
-
 export function useEmotionCache() {
 	const { i18n } = useTranslation();
-	if (i18n.language === "fa-IR") {
-		return rtlCache;
-	} else {
-		return ltrCache;
-	}
+
+	return createCache({
+		key: "mui",
+		stylisPlugins: i18n.language === "fa-IR" ? [prefixer, rtlPlugin] : []
+	});
 }
